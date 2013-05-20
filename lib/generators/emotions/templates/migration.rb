07dfc1c5133487741c3b2ce76431c60a410f3e73
Rémi Prévost <remi@exomel.com>
2013-05-20 17:40:05 -0400

    Fix typos in generator

--- lib/generators/emotions/templates/migration.rb
+++ lib/generators/emotions/templates/migration.rb
@@ -15,8 +15,9 @@ class AddEmotions < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :emotions, [:emotional_type, :emotional_id, :emotional_type, :emotional_id, :emotion], unique: true, name: 'index_emotions_by_emotional_emotive_and_emotion'
-    add_index :emotions, [:emotional_type, :emotional_id, :emotional_type, :emotional_id], name: 'index_emotions_by_emotional_and_emotive'
+    add_index :emotions, [:emotional_type, :emotional_id, :emotive_type, :emotive_id, :emotion], unique: true, name: 'index_emotions_by_emotional_emotive_and_emotion'
+    add_index :emotions, [:emotional_type, :emotional_id, :emotive_type, :emotive_id], name: 'index_emotions_by_emotional_and_emotive'
+    add_index :emotions, [:emotive_type, :emotive_id, :emotion], name: 'index_emotions_by_emotional_and_emotive'
   end
 
   def down

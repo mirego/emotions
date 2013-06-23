04a4d868b5bdad9f129bee2d32eb6e3c621a24b4
Rémi Prévost <remi@exomel.com>
2013-06-23 01:45:28 -0400

    Fix duplicate index in migration

--- lib/generators/emotions/templates/migration.rb
+++ lib/generators/emotions/templates/migration.rb
@@ -15,7 +15,7 @@ class AddEmotions < ActiveRecord::Migration
 
     add_index :emotions, [:emotional_type, :emotional_id, :emotive_type, :emotive_id, :emotion], unique: true, name: 'index_emotions_by_emotional_emotive_and_emotion'
     add_index :emotions, [:emotional_type, :emotional_id, :emotive_type, :emotive_id], name: 'index_emotions_by_emotional_and_emotive'
-    add_index :emotions, [:emotive_type, :emotive_id, :emotion], name: 'index_emotions_by_emotional_and_emotive'
+    add_index :emotions, [:emotive_type, :emotive_id, :emotion], name: 'index_emotions_by_emotive'
   end
 
   def down

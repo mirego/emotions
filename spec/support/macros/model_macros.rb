f96200e6ad809f5cd7ee70a38c23fe10ecb4e716
Rémi Prévost <remi@exomel.com>
2013-06-23 09:47:34 -0400

    Improve model macros

--- spec/support/macros/model_macros.rb
+++ spec/support/macros/model_macros.rb
@@ -1,15 +1,17 @@
 module ModelMacros
   # Create a new emotional model
-  def emotional(klass_name)
-    spawn_model klass_name do
+  def emotional(klass_name, &block)
+    spawn_model klass_name, ActiveRecord::Base do
       acts_as_emotional
+      instance_exec(&block) if block
     end
   end
 
   # Create a new emotive model
-  def emotive(klass_name)
-    spawn_model klass_name do
+  def emotive(klass_name, &block)
+    spawn_model klass_name, ActiveRecord::Base do
       acts_as_emotive
+      class_eval(&block) if block
     end
   end
 
@@ -21,9 +23,9 @@ module ModelMacros
 protected
 
   # Create a new model class
-  def spawn_model(klass_name, &block)
+  def spawn_model(klass_name, parent_klass, &block)
     Object.instance_eval { remove_const klass_name } if Object.const_defined?(klass_name)
-    Object.const_set(klass_name, Class.new(ActiveRecord::Base))
+    Object.const_set(klass_name, Class.new(parent_klass))
     Object.const_get(klass_name).class_eval(&block) if block_given?
   end
 end

6dfd74b2b9d81318b9ae23bc7bc3ac99354ed42c
Rémi Prévost <remi@exomel.com>
2013-06-24 20:45:25 -0400

    Refactor a few dynamic methods
    Make sure that most of the important code is stored in a regular method
    and not in dynamic methods. This adds a level of complexity we don't
    need.

--- lib/emotions/emotional.rb
+++ lib/emotions/emotional.rb
@@ -10,68 +10,107 @@ module Emotions
       end
     end
 
+    # @private
     def _emotions_about(emotive)
       self.emotions.where(emotive_id: emotive.id, emotive_type: emotive.class.name)
     end
 
+    # Return all emotions expressed by the emotional record
+    # towards another emotive record
+    #
+    # @example
+    #   user = User.first
+    #   picture = Picture.first
+    #   user.express! :happy, picture
+    #   user.emotions_about(picture)
+    #   # => [:happy]
     def emotions_about(emotive)
       _emotions_about(emotive).pluck(:emotion).map(&:to_sym)
     end
 
+    # Express an emotion towards another record
+    #
+    # @example
+    #   user = User.first
+    #   picture = Picture.first
+    #   user.express! :happy, picture
     def express!(emotion, emotive)
-      send("#{emotion}_about!", emotive)
+      emotion = _emotions_about(emotive).where(emotion: emotion).first_or_initialize
+
+      begin
+        emotion.tap(&:save!)
+      rescue ActiveRecord::RecordInvalid => e
+        raise InvalidEmotion.new(e.record)
+      end
     end
 
+    # No longer express an emotion towards another record
+    #
+    # @example
+    #   user = User.first
+    #   picture = Picture.first
+    #   user.no_longer_express! :happy, picture
     def no_longer_express!(emotion, emotive)
-      send("no_longer_#{emotion}_about!", emotive)
+      _emotions_about(emotive).where(emotion: emotion).first.tap { |e| e.try(:destroy) }
     end
 
     module ClassMethods
+      # Return an `ActiveRecord::Relation` containing the emotional records
+      # that expressed a specific emotion towards an emotive record
+      #
+      # @example
+      #   user = User.first
+      #   picture = Picture.first
+      #   user.express! :happy, picture
+      #   User.emotional_about(:happy, picture)
+      #   # => #<ActiveRecord::Relation [#<User id=1>]>
+      def emotional_about(emotion, emotive)
+        if emotive.class.emotive?
+          emotional_ids = emotive.emotions.where(emotion: emotion).where(emotional_type: self.name).pluck(:emotional_id)
+          where(id: emotional_ids)
+        else
+          # ActiveRecord 4 supports `.none`, not ActiveRecord 3
+          respond_to?(:none) ? none : where('1 = 0')
+        end
+      end
+
       def define_emotion_methods(emotion)
         class_eval <<-RUBY, __FILE__, __LINE__ + 1
           def #{emotion}_about?(emotive)
             !!#{emotion}_about(emotive).exists?
           end
-          alias #{emotion}? #{emotion}_about?
-          alias #{emotion}_with? #{emotion}_about?
-          alias #{emotion}_over? #{emotion}_about?
 
           def #{emotion}_about!(emotive)
-            emotion = #{emotion}_about(emotive).first_or_initialize
-
-            begin
-              emotion.tap(&:save!)
-            rescue ActiveRecord::RecordInvalid => e
-              raise InvalidEmotion.new(e.record)
-            end
+            express! #{emotion.inspect}, emotive
           end
-          alias #{emotion}_with! #{emotion}_about!
-          alias #{emotion}_over! #{emotion}_about!
 
           def no_longer_#{emotion}_about!(emotive)
-            #{emotion}_about(emotive).first.tap { |e| e.try(:destroy) }
+            no_longer_express! #{emotion.inspect}, emotive
           end
-          alias no_longer_#{emotion}_with! no_longer_#{emotion}_about!
-          alias no_longer_#{emotion}_over! no_longer_#{emotion}_about!
 
           def #{emotion}_about(emotive)
             _emotions_about(emotive).where(emotion: #{emotion.to_s.inspect})
           end
+
+          alias #{emotion}? #{emotion}_about?
+          alias #{emotion}_with? #{emotion}_about?
+          alias #{emotion}_over? #{emotion}_about?
+
+          alias #{emotion}_with! #{emotion}_about!
+          alias #{emotion}_over! #{emotion}_about!
+
+          alias no_longer_#{emotion}_with! no_longer_#{emotion}_about!
+          alias no_longer_#{emotion}_over! no_longer_#{emotion}_about!
+
           alias #{emotion}_with #{emotion}_about
           alias #{emotion}_over #{emotion}_about
-
         RUBY
 
         instance_eval <<-RUBY, __FILE__, __LINE__ + 1
           def #{emotion}_about(emotive)
-            if emotive.class.emotive?
-              emotional_ids = emotive.#{emotion}_about.where(emotional_type: self.name).pluck(:emotional_id)
-              where(id: emotional_ids)
-            else
-              # ActiveRecord 4 supports `.none`, not ActiveRecord 3
-              respond_to?(:none) ? none : where('1 = 0')
-            end
+            emotional_about(#{emotion.inspect}, emotive)
           end
+
           alias #{emotion}_with #{emotion}_about
           alias #{emotion}_over #{emotion}_about
         RUBY
